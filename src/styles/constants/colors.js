b3320cf9a29c86d162801b13d2dddfa547c3ca05
eduardo0402 <eduarjavier0402@gmail.com>
2023-01-13 17:00:04 -0500

    update: color palette.

--- src/styles/constants/colors.js
+++ src/styles/constants/colors.js
@@ -5,10 +5,12 @@ export default {
   warning: "#f5c40d",
   black: "#001529",
   dark: "#434343",
-  light: "#d9d9d9",
+  light: "#5C5B5F",
   menu: "#202020",
   gray: "#e8e8e8",
   white: "#ffffff",
   color_success: "#00e676",
   color_cancel: "#EF5350",
 };
+// border: 2px solid #5C5B5F !important;
+// background: #5C5B5F !important;
