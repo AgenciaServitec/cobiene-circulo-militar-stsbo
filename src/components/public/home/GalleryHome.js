d0f87a84313b04c2e55cce86208e531b8776e11f
eduardo0402 <eduarjavier0402@gmail.com>
2023-06-11 17:06:51 -0500

    upp date gallery Home

--- src/components/public/home/GalleryHome.js
+++ src/components/public/home/GalleryHome.js
@@ -9,7 +9,21 @@ import {
 } from "../../../images";
 
 export const GalleryHome = () => {
-    const images = [{src:ImageGalleyHome1,title:"Reconocimiento al Personal Militar"},{src:ImageGalleyHome2, title: "Sobre nosotros"},{src:ImageGalleyHome3, title: "Areas de Ceremonias"},{src:ImageGalleyHome4, title: "Piscinas de las Sedes"},{src:ImageGalleyHome5, title: "Piscinas de las Sedes"},{src:ImageGalleyHome6, title: "Piscinas de las Sedes"},{src:ImageGalleyHome7, title: ""},{src:ImageGalleyHome8,title:""},{src:ImageGalleyHome9, title: "Piscinas de las Sedes"},{src:ImageGalleyHome10, title: "Areas Sociales"},{src:ImageGalleyHome11, title: "Areas de sociales"},{src:ImageGalleyHome12, title: "Areas de Eventos"},{src:ImageGalleyHome13, title: "oficinas sede chorrillos"},{src:ImageGalleyHome14, title: "oficinas sede chorrillos"}]
+    const images = [
+        {src:ImageGalleyHome1,title:"Reconocimiento al Personal Militar"},
+        {src:ImageGalleyHome2, title: "Sobre nosotros"},
+        {src:ImageGalleyHome3, title: "Areas de Ceremonias"},
+        {src:ImageGalleyHome4, title: "Piscinas de las Sedes"},
+        {src:ImageGalleyHome5, title: "Piscinas de las Sedes"},
+        {src:ImageGalleyHome6, title: "Piscinas de las Sedes"},
+        /*{src:ImageGalleyHome7, title: ""},
+        {src:ImageGalleyHome8,title:""},*/
+        {src:ImageGalleyHome9, title: "Piscinas de las Sedes"},
+        {src:ImageGalleyHome10, title: "Areas Sociales"},
+        {src:ImageGalleyHome11, title: "Areas de sociales"},
+        {src:ImageGalleyHome12, title: "Areas de Eventos"},
+        {src:ImageGalleyHome13, title: "oficinas sede chorrillos"},
+        {src:ImageGalleyHome14, title: "oficinas sede chorrillos"}]
     return (
         <div>
             <Gallery images={images} title="GALERIA DE SEDES"/>
