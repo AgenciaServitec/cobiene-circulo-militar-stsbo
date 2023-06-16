140d04e846e8e721421268354f5c329e410e00af
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-16 18:08:42 -0500

    refactoring images

--- src/images/index.js
+++ src/images/index.js
@@ -1,38 +1,13 @@
 // ------Img Servitec-------
 export { default as LogoCobiene } from "./others/circulosts.webp";
-export { default as ExtintoresBackground } from "./others/newfondosts.webp";
-export { default as OffersServitec } from "./others/offers1.webp";
-export { default as ServitecSlider1 } from "./others/servitec-slider2.webp";
-export { default as ServitecSlider4 } from "./others/servitec-slider4.webp";
-export { default as ServitecSlider2 } from "./others/servitec-slider1.webp";
-export { default as ServitecSlider3 } from "./others/servitec-slider3.webp";
 export { default as ImgCongratulation } from "./others/congratulation.webp";
-export {default as Linea} from "./others/lineu.webp";
-
-// ------Img Servitec Factura Facil-------
-export { default as ImgLogoFacturaFacil } from "./others/logo-factura.webp";
-export { default as ImgCarousel1 } from "./others/img-carousel-girl.webp";
-export { default as ImgAbout } from "./others/facturacion-electronica-peru-sunat.webp";
-
-
-// ------Img Servitec AboutUs ALL IN ONE-------
-export { default as MarcaMitsubishi } from "./others/mitsubishi.webp";
-export { default as MarcaMac } from "./others/logmac-removebg-preview.webp";
+export { default as Linea } from "./others/lineu.webp";
+export { default as NewLogoCobiene } from "./default-images/newlogosts.webp";
 
 // ------Img Servitec Servicios ALL IN ONE-------
 export { default as Services1 } from "./others/sedechorrillos.webp";
 export { default as Services2 } from "./others/sedetarapacareview.webp";
 export { default as Services3 } from "./others/sederimacnew.webp";
-
-
-export { default as Product1 } from "./others/1primero.webp";
-export { default as Product2 } from "./others/2segundo.webp";
-export { default as Product3 } from "./others/3tercero.webp";
-export { default as Product4 } from "./others/4cuarto.webp";
-export { default as Product5 } from "./others/5quinto.webp";
-
-export { default as ProductServ1 } from "./others/traversador.webp";
-
 
 // -------SVG-------
 export * from "./svg/Phone";
@@ -41,120 +16,117 @@ export * from "./svg/Location";
 
 // -------Multimedia------
 export { default as videoAllinOne } from "./multimedia/video1.mp4";
-export { default as videAboutUs} from './multimedia/vid-about-us.mp4';
+export { default as videAboutUs } from "./multimedia/vid-about-us.mp4";
 
 // ----- SEDE CHORRILLOS------
-export {default as Chorrillos1} from './others/1chorillos.webp';
-export {default as Chorrillos2} from './others/2chorrillos.webp';
-export {default as Chorrillos3} from './others/3chorrillos.webp';
-export {default as Chorrillos4} from './others/4chorrillos.webp';
-export {default as Chorrillos5} from './others/7choriillos.webp';
-export {default as Chorrillos6} from './others/6chorrillos.webp';
-export {default as Chorrillos7} from './others/9chorrillos.webp';
-export {default as Chorrillos8} from "./others/7d143b0d-823e-4cc4-9a06-8a9fc89b1083.webp";
-export {default as Chorrillos9} from "./others/9b6256a0-51ca-4d69-99ae-ea8df1791090.webp";
-export {default as Chorrillos10} from "./others/104bd851-7823-4d2f-b3f0-b73ca129587b.webp";
-export {default as Chorrillos11} from "./others/0380cf6a-8215-43ea-a0fe-0ca70fd435f7.webp";
-export {default as Chorrillos12} from "./others/e9e5a8f8-9b05-4c7b-93f0-f4a952e8cf08.webp";
-export {default as Chorrillos13} from "./others/e023a444-6bb1-422e-bb19-e559a5aed180.webp";
-export {default as Chorrillos14} from "./others/f85a722e-3056-48db-8126-485149a596fa.webp";
-
+export { default as Chorrillos1 } from "./others/1chorillos.webp";
+export { default as Chorrillos2 } from "./others/2chorrillos.webp";
+export { default as Chorrillos3 } from "./others/3chorrillos.webp";
+export { default as Chorrillos4 } from "./others/4chorrillos.webp";
+export { default as Chorrillos5 } from "./others/7choriillos.webp";
+export { default as Chorrillos6 } from "./others/6chorrillos.webp";
+export { default as Chorrillos7 } from "./others/9chorrillos.webp";
+export { default as Chorrillos8 } from "./others/7d143b0d-823e-4cc4-9a06-8a9fc89b1083.webp";
+export { default as Chorrillos9 } from "./others/9b6256a0-51ca-4d69-99ae-ea8df1791090.webp";
+export { default as Chorrillos10 } from "./others/104bd851-7823-4d2f-b3f0-b73ca129587b.webp";
+export { default as Chorrillos11 } from "./others/0380cf6a-8215-43ea-a0fe-0ca70fd435f7.webp";
+export { default as Chorrillos12 } from "./others/e9e5a8f8-9b05-4c7b-93f0-f4a952e8cf08.webp";
+export { default as Chorrillos13 } from "./others/e023a444-6bb1-422e-bb19-e559a5aed180.webp";
+export { default as Chorrillos14 } from "./others/f85a722e-3056-48db-8126-485149a596fa.webp";
 
 //---------SEDE RIMAC -------
-export {default as Rimac1} from './others/1rimac.webp';
-export {default as Rimac2} from './others/2rimac.webp';
-export {default as Rimac3} from './others/3rimac.webp';
-export {default as Rimac4} from './others/4rimac.webp';
-export {default as Rimac5} from './others/5rimac.webp';
-export {default as Rimac6} from './others/6rimac.webp';
-export {default as Rimac7} from './others/3d7206a2-d0b9-4e90-8b94-8bd3589a07c6.webp';
-export {default as Rimac8} from './others/6f8e6e56-e5d6-4564-8c3d-bd211fafa101.webp';
-export {default as Rimac9} from './others/6fa1ba4c-fb8d-46e0-9b5f-a6cb6974ea18.webp';
-export {default as Rimac10} from './others/07e91dce-b753-4437-a55c-12fad8cf55a2.webp';
-export {default as Rimac11} from './others/8a4cfdb1-0992-4260-8134-0e664c045335.webp';
-export {default as Rimac12} from './others/135adb94-ac60-4a70-b9e9-8f17e43b42b0.webp';
-export {default as Rimac13} from './others/437a9be3-deec-4420-b748-ab394c7438fa.webp';
-export {default as Rimac14} from './others/893b7423-a616-4378-b57b-64adb82813f4.webp';
-export {default as Rimac15} from './others/a6b8c754-1657-4fae-8bec-d10d2f83b393.webp';
-export {default as Rimac16} from './others/a81100ad-2e40-4516-ba26-d45fff93949c.webp';
-export {default as Rimac17} from './others/c3915452-8618-4fef-b757-361fbb3dc15a.webp';
-export {default as Rimac18} from './others/cbab9cb4-fd22-4e69-ade7-caf80b9b5b10.webp';
-export {default as Rimac19} from './others/e618da39-3a54-43c5-b2c6-f83410067d8d-1.webp';
-export {default as Rimac20} from './others/ee9cfd9d-e08f-45ff-b741-c5f598bc8c6f.webp';
-export {default as Rimac21} from './others/whatsapp-image-2023-03-07-at-81119-pm.webp';
-export {default as Rimac22} from './others/6fed2056-5d80-4b01-9176-b74108de3c8b.webp';
-
+export { default as Rimac1 } from "./others/1rimac.webp";
+export { default as Rimac2 } from "./others/2rimac.webp";
+export { default as Rimac3 } from "./others/3rimac.webp";
+export { default as Rimac4 } from "./others/4rimac.webp";
+export { default as Rimac5 } from "./others/5rimac.webp";
+export { default as Rimac6 } from "./others/6rimac.webp";
+export { default as Rimac7 } from "./others/3d7206a2-d0b9-4e90-8b94-8bd3589a07c6.webp";
+export { default as Rimac8 } from "./others/6f8e6e56-e5d6-4564-8c3d-bd211fafa101.webp";
+export { default as Rimac9 } from "./others/6fa1ba4c-fb8d-46e0-9b5f-a6cb6974ea18.webp";
+export { default as Rimac10 } from "./others/07e91dce-b753-4437-a55c-12fad8cf55a2.webp";
+export { default as Rimac11 } from "./others/8a4cfdb1-0992-4260-8134-0e664c045335.webp";
+export { default as Rimac12 } from "./others/135adb94-ac60-4a70-b9e9-8f17e43b42b0.webp";
+export { default as Rimac13 } from "./others/437a9be3-deec-4420-b748-ab394c7438fa.webp";
+export { default as Rimac14 } from "./others/893b7423-a616-4378-b57b-64adb82813f4.webp";
+export { default as Rimac15 } from "./others/a6b8c754-1657-4fae-8bec-d10d2f83b393.webp";
+export { default as Rimac16 } from "./others/a81100ad-2e40-4516-ba26-d45fff93949c.webp";
+export { default as Rimac17 } from "./others/c3915452-8618-4fef-b757-361fbb3dc15a.webp";
+export { default as Rimac18 } from "./others/cbab9cb4-fd22-4e69-ade7-caf80b9b5b10.webp";
+export { default as Rimac19 } from "./others/e618da39-3a54-43c5-b2c6-f83410067d8d-1.webp";
+export { default as Rimac20 } from "./others/ee9cfd9d-e08f-45ff-b741-c5f598bc8c6f.webp";
+export { default as Rimac21 } from "./others/whatsapp-image-2023-03-07-at-81119-pm.webp";
+export { default as Rimac22 } from "./others/6fed2056-5d80-4b01-9176-b74108de3c8b.webp";
 
 //------ Backgrounds-------
-export {default as BackgroundChorrillos} from './others/sedechorrillosfondo.webp';
-export {default as BackgroundRimac} from './others/sederimacfondo.webp';
-export {default as BackgroundTarapaca} from './others/sedetarapacafondo.webp';
+export { default as BackgroundChorrillos } from "./others/sedechorrillosfondo.webp";
+export { default as BackgroundRimac } from "./others/sederimacfondo.webp";
+export { default as BackgroundTarapaca } from "./others/sedetarapacafondo.webp";
 
 //----- Backgrounds of slider ------
-export { default as SliderHome2} from './others/2sts-bacnew.webp';
-export {default as SliderHome3} from './others/2newbackgroundsts.webp';
-export {default as SliderHome4} from './others/newbackgroundsts.webp';
-export {default as SliderHom5} from './others/3newbackgroundsts.webp';
+export { default as SliderHome2 } from "./others/2sts-bacnew.webp";
+export { default as SliderHome3 } from "./others/2newbackgroundsts.webp";
+export { default as SliderHome4 } from "./others/newbackgroundsts.webp";
+export { default as SliderHom5 } from "./others/3newbackgroundsts.webp";
 
 //-----Novedades DEL CIRCULO MILITAR ------
 
-export { default as CobieneAppNovelty} from './others/noveltycobiene.webp';
-export { default as CobieneWebNovelty} from './others/noveltycobieneweb.webp';
-export { default as TramitesNovely} from './others/noveltycobienetramites.webp';
-export { default as CatalogoNovely} from './others/noveltycatalogo.webp';
-export {default as PdfNovely} from "./others/captura-pdf.webp";
-export {default as FacebookSede} from "./others/captura-facebook-sede.webp";
+export { default as CobieneAppNovelty } from "./others/noveltycobiene.webp";
+export { default as CobieneWebNovelty } from "./others/noveltycobieneweb.webp";
+export { default as TramitesNovely } from "./others/noveltycobienetramites.webp";
+export { default as CatalogoNovely } from "./others/noveltycatalogo.webp";
+export { default as PdfNovely } from "./others/captura-pdf.webp";
+export { default as FacebookSede } from "./others/captura-facebook-sede.webp";
 //-----Gallery of home ------
 
-export { default as ImageGalleyHome1} from './others/imghomesedes.webp';
-export { default as ImageGalleyHome2} from './others/imghomesedes2.webp';
-export { default as ImageGalleyHome3} from './others/imghomesedes3.webp';
-export { default as ImageGalleyHome4} from './others/imghomesedes4.webp';
-export { default as ImageGalleyHome5} from './others/imghomesedes5.webp';
-export { default as ImageGalleyHome6} from './others/imghomesedes6.webp';
-export { default as ImageGalleyHome7} from './others/imghomesedes7.webp';
-export { default as ImageGalleyHome8} from './others/imghomesedes8.webp';
-export { default as ImageGalleyHome9} from './others/imghomesedes9.webp';
-export { default as ImageGalleyHome10} from './others/imghomesedes10.webp';
-export { default as ImageGalleyHome11} from './others/imghomesedes11.webp';
-export { default as ImageGalleyHome12} from './others/imghomesedes12.webp';
-export { default as ImageGalleyHome13} from './others/imghomesedes13.webp';
-export { default as ImageGalleyHome14} from './others/imghomesedes14.webp';
+export { default as ImageGalleyHome1 } from "./others/imghomesedes.webp";
+export { default as ImageGalleyHome2 } from "./others/imghomesedes2.webp";
+export { default as ImageGalleyHome3 } from "./others/imghomesedes3.webp";
+export { default as ImageGalleyHome4 } from "./others/imghomesedes4.webp";
+export { default as ImageGalleyHome5 } from "./others/imghomesedes5.webp";
+export { default as ImageGalleyHome6 } from "./others/imghomesedes6.webp";
+export { default as ImageGalleyHome7 } from "./others/imghomesedes7.webp";
+export { default as ImageGalleyHome8 } from "./others/imghomesedes8.webp";
+export { default as ImageGalleyHome9 } from "./others/imghomesedes9.webp";
+export { default as ImageGalleyHome10 } from "./others/imghomesedes10.webp";
+export { default as ImageGalleyHome11 } from "./others/imghomesedes11.webp";
+export { default as ImageGalleyHome12 } from "./others/imghomesedes12.webp";
+export { default as ImageGalleyHome13 } from "./others/imghomesedes13.webp";
+export { default as ImageGalleyHome14 } from "./others/imghomesedes14.webp";
 
 //-----Gallery of Tarapaca ------
 
-export { default as Tarapaca1} from './others/localtarapaca.webp';
-export { default as ImageGalleryTarapaca1} from './others/localtarapaca1.webp';
-export { default as ImageGalleryTarapaca2} from './others/localtarapaca2.webp';
-export { default as Tarapaca3} from './others/40b34ba1-26c8-43c6-9805-6fa71d8df627.webp';
-export { default as Tarapaca4} from './others/63e77912-d380-4d36-9e18-198b3530dec8.webp';
-export { default as Tarapaca5} from './others/1994f210-7e8a-4c18-bc2c-79b500c60179.webp';
-export { default as Tarapaca6} from './others/0217977e-7313-4515-8df7-a3ab2546b240.webp';
-export { default as Tarapaca7} from './others/0217977e-7313-4515-8df7-a3ab2546b240-1.webp';
-export { default as Tarapaca8} from './others/b1a26857-7836-468c-a6d6-f707b2dba9c1.webp';
-export { default as Tarapaca9} from './others/bd5d55dc-de28-4677-af1e-a960b4f461cd.webp';
-export { default as Tarapaca10} from './others/d7be4089-fde0-4789-b8c2-e0ef1a2a53b4.webp';
-export { default as Tarapaca11} from './others/d54e4fc8-9766-41f1-9f04-9ae483752632.webp';
-export { default as Tarapaca12} from './others/40b34ba1-26c8-43c6-9805-6fa71d8df627-1.webp';
-
+export { default as Tarapaca1 } from "./others/localtarapaca.webp";
+export { default as ImageGalleryTarapaca1 } from "./others/localtarapaca1.webp";
+export { default as ImageGalleryTarapaca2 } from "./others/localtarapaca2.webp";
+export { default as Tarapaca3 } from "./others/40b34ba1-26c8-43c6-9805-6fa71d8df627.webp";
+export { default as Tarapaca4 } from "./others/63e77912-d380-4d36-9e18-198b3530dec8.webp";
+export { default as Tarapaca5 } from "./others/1994f210-7e8a-4c18-bc2c-79b500c60179.webp";
+export { default as Tarapaca6 } from "./others/0217977e-7313-4515-8df7-a3ab2546b240.webp";
+export { default as Tarapaca7 } from "./others/0217977e-7313-4515-8df7-a3ab2546b240-1.webp";
+export { default as Tarapaca8 } from "./others/b1a26857-7836-468c-a6d6-f707b2dba9c1.webp";
+export { default as Tarapaca9 } from "./others/bd5d55dc-de28-4677-af1e-a960b4f461cd.webp";
+export { default as Tarapaca10 } from "./others/d7be4089-fde0-4789-b8c2-e0ef1a2a53b4.webp";
+export { default as Tarapaca11 } from "./others/d54e4fc8-9766-41f1-9f04-9ae483752632.webp";
+export { default as Tarapaca12 } from "./others/40b34ba1-26c8-43c6-9805-6fa71d8df627-1.webp";
 
 //-----Gallery of VIDEOS IN SEDES ------
 
-export {default as VideoChorrillos1} from "./multimedia/videos-sedes/VID-20230308-WA0002.mp4";
-export {default as VideoChorrillos2} from "./multimedia/videos-sedes/VID-20230308-WA0013.mp4";
-export {default as VideoChorrillos3} from "./multimedia/videos-sedes/VID-20230308-WA0024.mp4";
-export {default as VideoChorrillos4} from "./multimedia/videos-sedes/VID-20230308-WA0027.mp4";
-export {default as VideoChorrillos5} from "./multimedia/videos-sedes/VID-20230308-WA0037.mp4";
-export {default as VideoChorrillos6} from "./multimedia/videos-sedes/VID-20230308-WA0038.mp4";
-export {default as VideoChorrillos7} from "./multimedia/videos-sedes/VID-20230308-WA0039.mp4";
-export {default as VideoChorrillos8} from "./multimedia/videos-sedes/VID-20230308-WA0040.mp4";
-export {default as VideoChorrillos9} from "./multimedia/videos-sedes/VID-20230308-WA0041.mp4";
-export {default as VideoChorrillos10} from "./multimedia/videos-sedes/VID-20230308-WA0043.mp4";
-export {default as VideoChorrillos11} from "./multimedia/videos-sedes/VID-20230308-WA0044.mp4";
-export {default as VideoChorrillos12} from "./multimedia/videos-sedes/VID-20230308-WA0045.mp4";
-export {default as VideoChorrillos13} from "./multimedia/videos-sedes/VID-20230308-WA0049.mp4";
-export {default as VideoChorrillos14} from "./multimedia/videos-sedes/WhatsApp Video 2023-02-21 at 2.01.49 PM.mp4";
-export {default as VideoChorrillos15} from "./multimedia/videos-sedes/WhatsApp Video 2023-02-27 at 10.22.48 AM.mp4";
-export {default as VideoChorrillos16} from "./multimedia/videos-sedes/WhatsApp Video 2023-03-08 at 5.07.40 AM (1).mp4";
-export {default as VideoChorrillos17} from "./multimedia/videos-sedes/tarapacaWhatsApp Video 2023-03-08 at 5.07.39 AM.mp4";
-export {default as VideoChorrillos18} from "./multimedia/videos-sedes/WhatsApp Video 2023-03-08 at 5.07.40 AM.mp4";
+export { default as VideoChorrillos1 } from "./multimedia/videos-sedes/VID-20230308-WA0002.mp4";
+export { default as VideoChorrillos2 } from "./multimedia/videos-sedes/VID-20230308-WA0013.mp4";
+export { default as VideoChorrillos3 } from "./multimedia/videos-sedes/VID-20230308-WA0024.mp4";
+export { default as VideoChorrillos4 } from "./multimedia/videos-sedes/VID-20230308-WA0027.mp4";
+export { default as VideoChorrillos5 } from "./multimedia/videos-sedes/VID-20230308-WA0037.mp4";
+export { default as VideoChorrillos6 } from "./multimedia/videos-sedes/VID-20230308-WA0038.mp4";
+export { default as VideoChorrillos7 } from "./multimedia/videos-sedes/VID-20230308-WA0039.mp4";
+export { default as VideoChorrillos8 } from "./multimedia/videos-sedes/VID-20230308-WA0040.mp4";
+export { default as VideoChorrillos9 } from "./multimedia/videos-sedes/VID-20230308-WA0041.mp4";
+export { default as VideoChorrillos10 } from "./multimedia/videos-sedes/VID-20230308-WA0043.mp4";
+export { default as VideoChorrillos11 } from "./multimedia/videos-sedes/VID-20230308-WA0044.mp4";
+export { default as VideoChorrillos12 } from "./multimedia/videos-sedes/VID-20230308-WA0045.mp4";
+export { default as VideoChorrillos13 } from "./multimedia/videos-sedes/VID-20230308-WA0049.mp4";
+export { default as VideoChorrillos14 } from "./multimedia/videos-sedes/WhatsApp Video 2023-02-21 at 2.01.49 PM.mp4";
+export { default as VideoChorrillos15 } from "./multimedia/videos-sedes/WhatsApp Video 2023-02-27 at 10.22.48 AM.mp4";
+export { default as VideoChorrillos16 } from "./multimedia/videos-sedes/WhatsApp Video 2023-03-08 at 5.07.40 AM (1).mp4";
+export { default as VideoChorrillos17 } from "./multimedia/videos-sedes/tarapacaWhatsApp Video 2023-03-08 at 5.07.39 AM.mp4";
+export { default as VideoChorrillos18 } from "./multimedia/videos-sedes/WhatsApp Video 2023-03-08 at 5.07.40 AM.mp4";
