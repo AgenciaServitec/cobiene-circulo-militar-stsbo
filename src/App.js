046b5708427f5c00e8e36be597482a6dc74c06ea
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-16 17:53:17 -0500

    formated code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { Router } from "./router";
 import { AuthenticationProvider } from "./providers";
-import {FormConsult} from "./components/public/ui/FormConsult";
-import {FormContactProvider} from "./providers";
+import { FormConsult } from "./components/public/ui/FormConsult";
+import { FormContactProvider } from "./providers";
 
 export const App = () => {
   const [visibleFormContact, setVisibleFormContact] = useState(false);
@@ -12,10 +12,10 @@ export const App = () => {
 
   return (
     <AuthenticationProvider>
-        <FormContactProvider>
-      <Router/>
-            <FormConsult />
-            </FormContactProvider>
+      <FormContactProvider>
+        <Router />
+        <FormConsult />
+      </FormContactProvider>
     </AuthenticationProvider>
   );
 };
