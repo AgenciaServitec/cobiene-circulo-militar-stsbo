13cb891913adbf29c25754790b35ecb84be1ec38
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-16 19:20:48 -0500

    add BoxAddress.js

---
+++ src/components/public/layout/BoxAddress.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+
+export const BoxAddress = () => {
+  return (
+    <Container>
+      <div className="item-address">
+        <span>
+          <a href="#">Av. Escuela Militar 990, chorrillos 15064</a>
+        </span>
+      </div>
+
+      <div className="item-icon">
+        <a href="#">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faWhatsapp} />
+        </a>
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background: #86895d;
+  width: 100%;
+  padding: 0.5em 1em;
+  display: flex;
+  justify-content: space-between;
+
+  .item-address {
+    span {
+      a {
+        color: #fff;
+        font-size: 0.6em;
+      }
+    }
+  }
+
+  .item-icon {
+    svg {
+      color: #fff;
+      font-size: 1.5em;
+      margin-left: 0.5em;
+    }
+  }
+`;
---
+++ src/components/public/layout/index.js
@@ -0,0 +1,5 @@
+export * from "./HeaderMobile";
+export * from "./HeaderDesktop";
+export * from "./Drawer";
+export * from "./Footer";
+export * from "./BoxAddress";
