23f44edba5067fe9ec5af693f67b13b275570732
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-20 13:20:34 -0500

    updated styles

--- src/components/public/home/AboutUs.js
+++ src/components/public/home/AboutUs.js
@@ -1,22 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import { mediaQuery } from "../../../styles/constants/mediaQuery";
-// import { useDevice } from "../../../hooks";
 
-export const AboutUs = ({title, styles, src}) => {
-  // const { isMobile } = useDevice();
+export const AboutUs = ({ title, styles, src }) => {
   return (
     <Container styles={styles} id="about-us">
-      <div className="wrapper-text">
-        <div className="content-text">
-            <h2 className="title">{title}</h2>
-          <p className="content">
-            La Asociación Círculo Militar de Supervisores Técnicos y SubOficiales del Ejército (ACM-STS), brinda bienestar al personal de Supervisores, Técnicos y subOficiales del Ejército del Perú en situación de Actividad, Disponibilidad o Retiro y sus FAMILIARES DIRECTOS, en el área de alojamiento, alimentaciones, recreación, deporte y cultura. Los beneficios que brinda a los ASOCIADOS son únicos y exclusivos para el: TITULAR Y SUS FAMILIARES DIRECTOS.
-          </p>
-        </div>
-        <div style={{alignSelf: "center", display: "flex", justifyContent: "center" }}>
-          <VideoBg autoPlay controls loop src={src} type="Video/mp4" />
-        </div>
+      <div className="content-text">
+        <h2 className="title">{title}</h2>
+        <p className="content">
+          La Asociación Círculo Militar de Supervisores Técnicos y SubOficiales
+          del Ejército (ACM-STS), brinda bienestar al personal de Supervisores,
+          Técnicos y subOficiales del Ejército del Perú en situación de
+          Actividad, Disponibilidad o Retiro y sus FAMILIARES DIRECTOS, en el
+          área de alojamiento, alimentaciones, recreación, deporte y cultura.
+          Los beneficios que brinda a los ASOCIADOS son únicos y exclusivos para
+          el: TITULAR Y SUS FAMILIARES DIRECTOS.
+        </p>
+      </div>
+      <div className="content-video">
+        <VideoBg autoPlay controls loop src={src} type="Video/mp4" />
       </div>
     </Container>
   );
@@ -25,67 +27,52 @@ export const AboutUs = ({title, styles, src}) => {
 const Container = styled.section`
   width: 100%;
   height: auto;
-  padding: ${({styles})=>styles.padding};
-  background: none repeat scroll 0 0 #ffffff;
-  text-align: center;
-  .title {
-    letter-spacing: 0.3px;
-    font-family: "Intro Demo", sans-serif;
+  display: grid;
+  grid-template-columns: 1fr;
+  align-items: center;
+  padding: 0 1.5em;
+  margin: 1.5em 0;
+  ${mediaQuery.minDesktop} {
+    grid-template-columns: 1fr auto;
+  }
+
+  .content-text {
+    text-align: left;
     width: 100%;
-    color: #F6B63E;
-    font-size: 2.5rem;
-    @media(max-width: 480px){
-      font-size: 2rem;
-    }
-  }
+    padding: 1.5em;
 
-  .wrapper-logos-mobile {
-    padding: 0.5rem 2rem;
-
-    .wrapper-logo-mobile {
-      max-width: 13em;
+    .title {
+      letter-spacing: 0.3px;
+      font-family: "Intro Demo", sans-serif;
       width: 100%;
-      padding: 2em 0 3.5em 0;
-
-      img {
-        width: 100%;
-      }
-    }
-  }
-
-  .wrapper-text {
-    background: none repeat scroll 0 0 #ffffff;
-    display: grid;
-    grid-template-columns: ${({styles})=> styles.grid};
-    ${mediaQuery.maxMobile} {
-      grid-template-columns: 1fr;
-      padding-bottom: 2rem;
-    }
-
-    .content-text {
-      //text-align: center;
-      text-align: left;
-      width: 100%;
-      padding: 0 2em 1.5em 2em;
-
+      color: #f6b63e;
+      font-size: 2.5em;
       ${mediaQuery.minTablet} {
-        padding: 2em 5rem 2rem 5rem;
-      }
-
-      .content {
-        font-size: 1.2rem;
-        line-height: 1.3em;
-        //margin-bottom: 2em;
-        margin-top: 1em;
-        text-align: justify;
-        //justify-content: ;
+        font-size: 2em;
       }
     }
+
+    .content {
+      font-size: 1.2rem;
+      line-height: 1.3em;
+      margin-top: 1em;
+      text-align: justify;
+    }
+  }
+
+  .content-video {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: auto;
   }
 `;
 
 const VideoBg = styled.video`
-  width: 70%;
+  padding: 1em;
   height: auto;
   background: #232a34;
+  width: 100%;
+  max-width: 30em;
 `;
