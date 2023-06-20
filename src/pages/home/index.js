87788acf116c3f95b4e2ecdcdefd1a63875b8b41
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-20 16:27:16 -0500

    remove comments

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,37 +1,22 @@
 import React from "react";
 import styled from "styled-components";
 import { Carousel, AboutUs, Sedes, GalleryHome } from "../../components/public";
-import {Services} from "../../components/public/home/Services";
-import {VideoChorrillos16} from "../../images";
+import { Services } from "../../components/public/home/Services";
+import { VideoChorrillos16 } from "../../images";
 export const Home = ({ onClickVisibleFormContact, visibleFormContact }) => {
   return (
     <>
       <Container>
-        {/*<Carousel*/}
-        {/*  onClickVisibleFormContact={onClickVisibleFormContact}*/}
-        {/*  onEventGaClickButton={onEventGaClickButton}*/}
-        {/*/>*/}
-          <Carousel />
-        {/*<InitialComponent*/}
-        {/*  bgImage={ExtintoresBackground}*/}
-        {/*  description="PATRIA, SOCIEDAD Y FAMILIA."*/}
-        {/*/>*/}
-        <AboutUs title="SOBRE NOSOTROS" src={VideoChorrillos16} styles={{padding: "4rem 0 0 0", grid:"1fr 1fr"}} />
-          {/*<CustomImage/>*/}
+        <Carousel />
+
+        <AboutUs title="SOBRE NOSOTROS" src={VideoChorrillos16} />
+
         <Sedes
           onClickVisibleFormContact={onClickVisibleFormContact}
           visibleFormContact={visibleFormContact}
         />
-          <GalleryHome/>
-          <Services/>
-        {/*<WrapperComponent>*/}
-        {/*  <VideoSection*/}
-        {/*    onClickVisibleFormContact={onClickVisibleFormContact}*/}
-        {/*    onEventGaClickButton={onEventGaClickButton}*/}
-        {/*    onEventGaClickVideos={onEventGaClickVideos}*/}
-        {/*  />*/}
-
-        {/*</WrapperComponent>*/}
+        <GalleryHome />
+        <Services />
       </Container>
     </>
   );
