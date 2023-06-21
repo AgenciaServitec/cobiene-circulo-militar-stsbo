91378b2f5e47566eefce5bad1d4288e3bce68364
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-20 19:22:31 -0500

    updated styles

--- src/components/public/layout/Footer.js
+++ src/components/public/layout/Footer.js
@@ -1,11 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  SvgEmail,
-  SvgLocation,
-  SvgPhone,
-  LogoCobiene,
-} from "../../../images";
+import { LogoCobiene, SvgEmail, SvgLocation, SvgPhone } from "../../../images";
 import { faFacebook, faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -14,153 +9,144 @@ import {
   faLocationDot,
 } from "@fortawesome/free-solid-svg-icons";
 import { mediaQuery } from "../../../styles/constants/mediaQuery";
-import { WrapperComponent } from "../ui";
 import { contactData } from "../../../firebase";
 import { capitalize } from "lodash";
 
 export const Footer = ({ onEventGaClickIcon, onEventGaClickLink }) => (
   <Container id="footer">
-    <WrapperComponent>
-      <>
-        <div className="item-logo-footer">
-          <div className="item-logo">
-            <img src={LogoCobiene} alt="Logo publicidad google" />
-          </div>
-          <div className="item-text">
-            <h3>¡Trabajando por su bienestar!</h3>
-            <h4 className="content-contact">
-              Contáctanos: 01 6449277
-            </h4>
-            <span className="content-somos">Circulo de SubOficiales</span>
-          </div>
-          <div className="items-socials">
-            <ul>
-              <li
-                onClick={() =>
-                  onEventGaClickIcon(
-                    "click-icon-wsp-941801827",
-                    "Click icon wsp 941 801 827"
-                  )
-                }
-              >
-                <a
-                  href="https://goo.gl/maps/JpygwD7qvhcNVU9E6"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faLocationDot} />
-                </a>
-              </li>
-              <li
-                onClick={() =>
-                  onEventGaClickIcon(
-                    "click-icon-wsp-941801827",
-                    "Click icon wsp 941 801 827"
-                  )
-                }
-              >
-                <a
-                  href="https://api.whatsapp.com/send/?phone=+51929054672&text=Estoy+interesado+%2F+servicio&app_absent=0"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faWhatsapp} />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="mailto:contacto@cobiene.mil.pe"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </a>
-              </li>
-              <li
-              >
-                <a
-                  href="https://www.facebook.com/ACMSTS"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faFacebook} />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="items-contacts">
-          <div className="item-contact">
-            <div className="icon">
-              <SvgPhone width="80px" />
-            </div>
-            <div
-              className="content"
-              onClick={() =>
-                onEventGaClickLink(
-                  "click-link-telefono-941801827",
-                  "Click link telefono 941801827"
-                )
-              }
+    <div className="item-logo-footer">
+      <div className="item-logo">
+        <img src={LogoCobiene} alt="Logo Circulo Militar" />
+      </div>
+      <div className="item-text">
+        <h3>¡Trabajando por su bienestar!</h3>
+        <h4 className="content-contact">Contáctanos: 01 6449277</h4>
+        <span className="content-somos">Circulo de SubOficiales</span>
+      </div>
+      <div className="items-socials">
+        <ul>
+          <li
+            onClick={() =>
+              onEventGaClickIcon(
+                "click-icon-wsp-941801827",
+                "Click icon wsp 941 801 827"
+              )
+            }
+          >
+            <a
+              href="https://goo.gl/maps/JpygwD7qvhcNVU9E6"
+              target="_blank"
+              rel="noreferrer"
             >
-              <a href="tel:+51929054672" target="_blank" rel="noreferrer">
-                929054672
-              </a>
-              <p> Lun-Dom 9am-6pm</p>
-            </div>
-          </div>
-
-          <div className="item-contact">
-            <div className="icon">
-              <SvgEmail width="80px" />
-            </div>
-            <div
-              className="content"
-              onClick={() =>
-                onEventGaClickLink(
-                  "click-link-email-contactos@servitec-peru.com",
-                  "Click link email contactos@servitec-peru.com"
-                )
-              }
+              <FontAwesomeIcon icon={faLocationDot} />
+            </a>
+          </li>
+          <li
+            onClick={() =>
+              onEventGaClickIcon(
+                "click-icon-wsp-941801827",
+                "Click icon wsp 941 801 827"
+              )
+            }
+          >
+            <a
+              href="https://api.whatsapp.com/send/?phone=+51929054672&text=Estoy+interesado+%2F+servicio&app_absent=0"
+              target="_blank"
+              rel="noreferrer"
             >
-              <a
-                href="mailto:contacto@cobiene.mil.pe"
-                target="_blank"
-                rel="noreferrer"
-              >
-                contacto@cobiene.mil.pe
-              </a>
-              <p>Soporte en Linea</p>
-            </div>
-          </div>
-
-          <div className="item-contact">
-            <div className="icon">
-              <SvgLocation width="80px" />
-            </div>
-            <div
-              className="content"
+              <FontAwesomeIcon icon={faWhatsapp} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="mailto:contacto@cobiene.mil.pe"
+              target="_blank"
+              rel="noreferrer"
             >
-              <a
-                href="https://goo.gl/maps/JpygwD7qvhcNVU9E6"
-                target="_blank"
-                rel="noreferrer"
-              >
-                Chorrillos, Perú
-              </a>
-              <p>Av. Escuela Militar 990, Chorrillos 15064</p>
-            </div>
-          </div>
+              <FontAwesomeIcon icon={faEnvelope} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/ACMSTS"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div className="items-contacts">
+      <div className="item-contact">
+        <div className="icon">
+          <SvgPhone width="80px" />
         </div>
-        <div className="bottom-footer">
-          <div>
-            © Copyright 2022 - {capitalize(contactData.name)} / Diseñado con{" "}
-            <FontAwesomeIcon icon={faHeart} color="red" /> por{" CiculaMilitarsts"}
-          </div>
+        <div
+          className="content"
+          onClick={() =>
+            onEventGaClickLink(
+              "click-link-telefono-941801827",
+              "Click link telefono 941801827"
+            )
+          }
+        >
+          <a href="tel:+51929054672" target="_blank" rel="noreferrer">
+            929054672
+          </a>
+          <p> Lun-Dom 9am-6pm</p>
         </div>
-      </>
-    </WrapperComponent>
+      </div>
+
+      <div className="item-contact">
+        <div className="icon">
+          <SvgEmail width="80px" />
+        </div>
+        <div
+          className="content"
+          onClick={() =>
+            onEventGaClickLink(
+              "click-link-email-contactos@servitec-peru.com",
+              "Click link email contactos@servitec-peru.com"
+            )
+          }
+        >
+          <a
+            href="mailto:contacto@cobiene.mil.pe"
+            target="_blank"
+            rel="noreferrer"
+          >
+            contacto@cobiene.mil.pe
+          </a>
+          <p>Soporte en Linea</p>
+        </div>
+      </div>
+
+      <div className="item-contact">
+        <div className="icon">
+          <SvgLocation width="80px" />
+        </div>
+        <div className="content">
+          <a
+            href="https://goo.gl/maps/JpygwD7qvhcNVU9E6"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Chorrillos, Perú
+          </a>
+          <p>Av. Escuela Militar 990, Chorrillos 15064</p>
+        </div>
+      </div>
+    </div>
+    <div className="bottom-footer">
+      <div>
+        © Copyright 2022 - {capitalize(contactData.name)} / Diseñado con
+        <FontAwesomeIcon icon={faHeart} color="red" /> por
+        {" CiculaMilitarsts"}
+      </div>
+    </div>
   </Container>
 );
 
@@ -176,18 +162,14 @@ const Container = styled.footer`
       display: flex;
       justify-content: center;
       img {
-        //display: none;
         width: 4rem;
         height: auto;
-        //margin: 0.3rem 0 1.2rem 0;
       }
     }
     .item-text {
       text-align: center;
-      //margin-bottom: 6rem;
       padding-top: 1rem;
       position: relative;
-      text-align: center;
       width: 100%;
       ${mediaQuery.maxMobile} {
         padding: 0.5em;
@@ -257,29 +239,30 @@ const Container = styled.footer`
     grid-template-columns: 1fr;
     justify-items: center;
 
-    ${mediaQuery.minTablet} {
-      grid-template-columns: 1fr 1fr 1fr;
+    ${mediaQuery.minDesktop} {
+      grid-template-columns: repeat(auto-fit, minmax(8em, auto));
     }
 
     .item-contact {
       display: grid;
       grid-template-columns: 1fr;
       justify-items: center;
-      gap: 1rem;
-      ${mediaQuery.minTablet} {
+      gap: 0.5em;
+      ${mediaQuery.minDesktop} {
         grid-template-columns: 1fr auto;
       }
       .icon {
-        font-size: 3rem;
+        font-size: 2em;
       }
       .content {
         text-align: center;
-        line-height: 3rem;
+        line-height: 2em;
+
         p {
           opacity: 0.4;
         }
         a {
-          font-size: 1.5rem;
+          font-size: 1.2em;
           color: #fff;
         }
         a:hover {
