cca2e03a22bca971293474ba49302cecf33241ae
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-22 16:17:49 -0500

    formated code

--- src/pages/sede-chorrillos/index.js
+++ src/pages/sede-chorrillos/index.js
@@ -1,99 +1,105 @@
-import React from 'react';
-import {Gallery, VideosSedes, AboutUs} from "../../components/public/home";
+import React from "react";
+import { Gallery, VideosSedes, AboutUs } from "../../components/public/home";
 import styled from "styled-components";
 import {
-    Chorrillos1,
-    Chorrillos2,
-    Chorrillos3,
-    Chorrillos4,
-    Chorrillos5,
-    Chorrillos6,
-    Chorrillos7,
-    Chorrillos8,
-    Chorrillos9,
-    Chorrillos10,
-    Chorrillos11,
-    Chorrillos12,
-    Chorrillos13,
-    Chorrillos14,
-    BackgroundChorrillos,
-    VideoChorrillos1,
-    VideoChorrillos2,
-    VideoChorrillos3,
-    VideoChorrillos4,
-    VideoChorrillos5,
-    VideoChorrillos6,
-    VideoChorrillos7,
-    VideoChorrillos8,
-    VideoChorrillos9,
-    VideoChorrillos10,
-    VideoChorrillos11,
-    VideoChorrillos12,
-    VideoChorrillos13,
-    VideoChorrillos14,
-    VideoChorrillos15,
-    VideoChorrillos16,
-} from '../../images';
-import {Contact, InitialComponent} from "../../components/public";
+  Chorrillos1,
+  Chorrillos2,
+  Chorrillos3,
+  Chorrillos4,
+  Chorrillos5,
+  Chorrillos6,
+  Chorrillos7,
+  Chorrillos8,
+  Chorrillos9,
+  Chorrillos10,
+  Chorrillos11,
+  Chorrillos12,
+  Chorrillos13,
+  Chorrillos14,
+  BackgroundChorrillos,
+  VideoChorrillos1,
+  VideoChorrillos2,
+  VideoChorrillos3,
+  VideoChorrillos4,
+  VideoChorrillos5,
+  VideoChorrillos6,
+  VideoChorrillos7,
+  VideoChorrillos8,
+  VideoChorrillos9,
+  VideoChorrillos10,
+  VideoChorrillos11,
+  VideoChorrillos12,
+  VideoChorrillos13,
+  VideoChorrillos14,
+  VideoChorrillos15,
+  VideoChorrillos16,
+} from "../../images";
+import { Contact, InitialComponent } from "../../components/public";
 
-export const PageSedeChorrillos = ({videosSedes}) => {
+export const PageSedeChorrillos = ({ videosSedes }) => {
+  const images = [
+    { src: Chorrillos1, title: "Areas Deportivas" },
+    { src: Chorrillos2, title: "Piscina" },
+    { src: Chorrillos3, title: "Áreas de la Sede" },
+    { src: Chorrillos4, title: "Áreas de la Sede" },
+    { src: Chorrillos5, title: "Areas de Reunion" },
+    { src: Chorrillos6, title: "Nuestra Sede" },
+    { src: Chorrillos7, title: "Areas de Eventos" },
+    { src: Chorrillos3, title: "Áreas de la Sede" },
+    { src: Chorrillos8, title: "Áreas Deportivas" },
+    { src: Chorrillos9, title: "Áreas de Eventos" },
+    { src: Chorrillos10, title: "Áreas Sociales" },
+    { src: Chorrillos11, title: "Áreas Sociales" },
+    { src: Chorrillos12, title: "Áreas de la Sede" },
+    { src: Chorrillos13, title: "Áreas Deportivas" },
+    { src: Chorrillos14, title: "Áreas Recreacionales" },
+  ];
 
+  const videos = [
+    { src: VideoChorrillos1, title: "video2" },
+    { src: VideoChorrillos2, title: "video2" },
+    { src: VideoChorrillos3, title: "video2" },
+    { src: VideoChorrillos4, title: "video2" },
+    { src: VideoChorrillos5, title: "video2" },
+    { src: VideoChorrillos6, title: "video2" },
+    { src: VideoChorrillos7, title: "video2" },
+    { src: VideoChorrillos8, title: "video2" },
+    { src: VideoChorrillos9, title: "video2" },
+    { src: VideoChorrillos10, title: "video2" },
+    { src: VideoChorrillos11, title: "video2" },
+    { src: VideoChorrillos12, title: "video2" },
+    { src: VideoChorrillos13, title: "video2" },
+    { src: VideoChorrillos14, title: "video2" },
+    { src: VideoChorrillos15, title: "video2" },
+    { src: VideoChorrillos16, title: "video2" },
+  ];
 
-    const images = [
-        { src:Chorrillos1, title:"Areas Deportivas"},
-        {src:Chorrillos2, title: "Piscina"},
-        {src:Chorrillos3, title:"Áreas de la Sede"},
-        {src:Chorrillos4,title: "Áreas de la Sede"},
-        {src:Chorrillos5, title: "Areas de Reunion"},
-        {src:Chorrillos6, title:"Nuestra Sede"},
-        {src:Chorrillos7, title:"Areas de Eventos"},
-        {src:Chorrillos3, title:"Áreas de la Sede"},
-        {src:Chorrillos8, title:"Áreas Deportivas"},
-        {src:Chorrillos9, title:"Áreas de Eventos"},
-        {src:Chorrillos10, title:"Áreas Sociales"},
-        {src:Chorrillos11, title:"Áreas Sociales"},
-        {src:Chorrillos12, title:"Áreas de la Sede"},
-        {src:Chorrillos13, title:"Áreas Deportivas"},
-        {src:Chorrillos14, title:"Áreas Recreacionales"},
-
-    ];
-
-    const videos = [
-        {src: VideoChorrillos1, title: "video2"},
-        {src: VideoChorrillos2, title: "video2"},
-        {src: VideoChorrillos3, title: "video2"},
-        {src: VideoChorrillos4, title: "video2"},
-        {src: VideoChorrillos5, title: "video2"},
-        {src: VideoChorrillos6, title: "video2"},
-        {src: VideoChorrillos7, title: "video2"},
-        {src: VideoChorrillos8, title: "video2"},
-        {src: VideoChorrillos9, title: "video2"},
-        {src: VideoChorrillos10, title: "video2"},
-        {src: VideoChorrillos11, title: "video2"},
-        {src: VideoChorrillos12, title: "video2"},
-        {src: VideoChorrillos13, title: "video2"},
-        {src: VideoChorrillos14, title: "video2"},
-        {src: VideoChorrillos15, title: "video2"},
-        {src: VideoChorrillos16, title: "video2"},
-
-    ]
-
-    return (
-        <>
-            <Container>
-                <InitialComponent bgImage={BackgroundChorrillos} description=""/>
-                <AboutUs src={VideoChorrillos16} title="Nosotros-Sede Chorrillos" styles={{padding: "2rem 0 2rem 0", grid:"60% 40%"}} />
-                {
-                    videosSedes ? (<VideosSedes videos={videos}  title="NUESTROS RECUERDOS" />) : ( <Gallery background="#efefef" images={images} title="ZONAS RECREATIVAS"/>)
-                }
-                <Contact ubicacion="chorrillos"/>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container>
+        <InitialComponent bgImage={BackgroundChorrillos} description="" />
+        <AboutUs
+          src={VideoChorrillos16}
+          title="Nosotros-Sede Chorrillos"
+          styles={{ padding: "2rem 0 2rem 0", grid: "60% 40%" }}
+        />
+        {videosSedes ? (
+          <VideosSedes videos={videos} title="NUESTROS RECUERDOS" />
+        ) : (
+          <Gallery
+            background="#efefef"
+            images={images}
+            title="ZONAS RECREATIVAS"
+          />
+        )}
+        <Contact location="chorrillos" />
+      </Container>
+    </>
+  );
 };
 
 const Container = styled.div`
-    width: 100%;
+  width: 100%;
   height: auto;
   background: white;
 `;
--- src/pages/sede-tarapaca/index.js
+++ src/pages/sede-tarapaca/index.js
@@ -1,63 +1,71 @@
 import React from "react";
 
 import styled from "styled-components";
-import {Contact, InitialComponent,AboutUs} from "../../components/public";
-import {BackgroundTarapaca,ImageGalleryTarapaca1,ImageGalleryTarapaca2,
-    Services2,
-    Tarapaca1,
-    Tarapaca3,
-    Tarapaca4,
-    Tarapaca5,
-    Tarapaca6,
-    Tarapaca7,
-    Tarapaca8,
-    Tarapaca9,
-    Tarapaca10,
-    Tarapaca11,
-    Tarapaca12,
-    VideoChorrillos17,
+import { Contact, InitialComponent, AboutUs } from "../../components/public";
+import {
+  BackgroundTarapaca,
+  ImageGalleryTarapaca1,
+  ImageGalleryTarapaca2,
+  Services2,
+  Tarapaca1,
+  Tarapaca3,
+  Tarapaca4,
+  Tarapaca5,
+  Tarapaca6,
+  Tarapaca7,
+  Tarapaca8,
+  Tarapaca9,
+  Tarapaca10,
+  Tarapaca11,
+  Tarapaca12,
+  VideoChorrillos17,
 } from "../../images";
-import {Gallery} from "../../components/public/home/Gallery";
+import { Gallery } from "../../components/public/home/Gallery";
 
 export const PageSedeTarapaca = () => {
-    const images = [
-        {
-            src: Services2, title:"Nuestra Sede"
-        },
-        {
-            src: Tarapaca1, title:"Areas de Eventos"
-        },
-        {
-            src: ImageGalleryTarapaca1, title:"Salón de eventos"
-        },
-        {src: ImageGalleryTarapaca2, title:"Areas de Eventos"},
-        {src: Tarapaca3, title:"Piscinas"},
-        {src: Tarapaca4, title:"Piscinas"},
-        {src: Tarapaca5, title:"Piscinas Recreacionales"},
-        {src: Tarapaca5, title:"Piscinas Recreacionales"},
-        {src: Tarapaca6, title:"Piscinas Recreacionales"},
-        {src: Tarapaca7, title:"Piscinas Recreacionales"},
-        {src: Tarapaca8, title:"Areas de Recreacion"},
-        {src: Tarapaca9, title:"Areas de Recreacion"},
-        {src: Tarapaca10, title:"Piscinas"},
-        {src: Tarapaca11, title:"Restaurantes"},
-        {src: Tarapaca11, title:"Restaurantes"},
-        {src: Tarapaca12, title:"Piscinas"},
-    ]
+  const images = [
+    {
+      src: Services2,
+      title: "Nuestra Sede",
+    },
+    {
+      src: Tarapaca1,
+      title: "Areas de Eventos",
+    },
+    {
+      src: ImageGalleryTarapaca1,
+      title: "Salón de eventos",
+    },
+    { src: ImageGalleryTarapaca2, title: "Areas de Eventos" },
+    { src: Tarapaca3, title: "Piscinas" },
+    { src: Tarapaca4, title: "Piscinas" },
+    { src: Tarapaca5, title: "Piscinas Recreacionales" },
+    { src: Tarapaca5, title: "Piscinas Recreacionales" },
+    { src: Tarapaca6, title: "Piscinas Recreacionales" },
+    { src: Tarapaca7, title: "Piscinas Recreacionales" },
+    { src: Tarapaca8, title: "Areas de Recreacion" },
+    { src: Tarapaca9, title: "Areas de Recreacion" },
+    { src: Tarapaca10, title: "Piscinas" },
+    { src: Tarapaca11, title: "Restaurantes" },
+    { src: Tarapaca11, title: "Restaurantes" },
+    { src: Tarapaca12, title: "Piscinas" },
+  ];
   return (
-      <div>
-          <Container>
-              <InitialComponent bgImage={BackgroundTarapaca} description=""/>
-              <AboutUs src={VideoChorrillos17} title="Nosotros-Sede Tarapacá" styles={{padding: "2rem 0 2rem 0rem", grid:"60% 40%"}} />
-              <Gallery images={images} title="Sede Tarapacá" background="#efefef"/>
-              <Contact ubicacion="chorrillos"/>
-          </Container>
-      </div>
+    <Container>
+      <InitialComponent bgImage={BackgroundTarapaca} description="" />
+      <AboutUs
+        src={VideoChorrillos17}
+        title="Nosotros-Sede Tarapacá"
+        styles={{ padding: "2rem 0 2rem 0rem", grid: "60% 40%" }}
+      />
+      <Gallery images={images} title="Sede Tarapacá" background="#efefef" />
+      <Contact location="chorrillos" />
+    </Container>
   );
 };
 
 const Container = styled.div`
   width: 100%;
   height: auto;
-  background: white;
+  background: #fff;
 `;
