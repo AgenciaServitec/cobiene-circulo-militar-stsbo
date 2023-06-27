f3b9ccd5687fce1faed8b580f05a936705c601dc
Elizabeth-mk <mhelizabeth3@gmail.com>
2023-06-27 17:43:01 -0500

    add imageFlipbook

---
+++ src/images/flip-book/index.js
@@ -0,0 +1,18 @@
+//img father day
+export { default as ImgFatherDay1 } from "./father-day/diaDelPadre1.webp";
+export { default as ImgFatherDay2 } from "./father-day/diaDelPadre2.webp.webp";
+export { default as ImgFatherDay3 } from "./father-day/diaDelPadre3.webp.webp";
+export { default as ImgFatherDay4 } from "./father-day/diaDelPadre4.webp";
+export { default as ImgFatherDay5 } from "./father-day/diaDelPadre5.webp";
+export { default as ImgFatherDay6 } from "./father-day/diaDelPadre6.webp";
+export { default as ImgFatherDay7 } from "./father-day/diaDelPadre7.webp";
+export { default as ImgFatherDay8 } from "./father-day/diaDelPadre8.webp";
+export { default as ImgFatherDay9 } from "./father-day/diaDelPadre9.webp";
+
+//img mother day
+export { default as ImgMother1 } from "./mother-day/diadelamadre1.webp";
+export { default as ImgMother2 } from "./mother-day/diadelamadre2.webp";
+export { default as ImgMother3 } from "./mother-day/diadelamadre3.webp";
+export { default as ImgMother4 } from "./mother-day/diadelamadre4.webp";
+
+
