c76efc4e910a71bfea672fc2dbf96e307256eb8a
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-27 15:55:31 -0500

    reformat code

--- src/components/public/home/CustomImage.js
+++ src/components/public/home/CustomImage.js
@@ -1,51 +1,59 @@
-import React, {useState} from 'react';
-import styled from 'styled-components';
-import {Services1, Services2, Services3, Services4} from "../../../images";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Services1, Services2, Services3 } from "../../../images";
 
 export const CustomImage = () => {
-    const imgs=[{id:0,value:Services1},{id:1, value: Services2},{id:2, value: Services3},{id:3,value:Services4}]
+  const images = [
+    { id: 0, value: Services1 },
+    { id: 1, value: Services2 },
+    { id: 2, value: Services3 },
+  ];
 
-    const [sliderData, setSliderData]=useState(imgs[0])
+  const [sliderData, setSliderData] = useState(images[0]);
 
-    const handleClick=(index)=>{
-        console.log(index);
-        const slider = imgs[index];
-        setSliderData(slider);
-    }
+  const handleClick = (index) => {
+    console.log(index);
+    const slider = images[index];
+    setSliderData(slider);
+  };
 
-    return (
-        <Container>
-
-            <div className="wrapper-slider">
-                <img src={sliderData.value} height="300" width="500"/>
-                <div className="conten-imgs">
-                    {
-                    imgs.map((img,i)=>
-                        <div className="conten-img">
-                           <div className="wrapper-img">
-                               <img className={sliderData.id===i ? "clicked" : "not-clicked"} key={i} src={img.value} onClick={()=>handleClick(i)} height="70" width="100"/>
-                           </div>
-                        </div>
-                    )
-                }</div>
+  return (
+    <Container>
+      <div className="wrapper-slider">
+        <img src={sliderData.value} height="300" width="500" loading="lazy" />
+        <div className="content-images">
+          {images.map((image, i) => (
+            <div className="content-img">
+              <div className="wrapper-img">
+                <img
+                  className={sliderData.id === i ? "clicked" : "not-clicked"}
+                  key={i}
+                  src={image.value}
+                  onClick={() => handleClick(i)}
+                  height="70"
+                  width="100"
+                  loading="lazy"
+                />
+              </div>
             </div>
-        </Container>
-    );
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
 };
 
-const Container = styled.div`
-text-align: center;
-  .wrapper-slider{
-    //display: grid;
-    //grid-template-rows: 1fr 1fr;
-    .conten-imgs{
-      justify-content: center;
+const Container = styled.article`
+  text-align: center;
+  .wrapper-slider {
+    .content-images {
+      justify-contentt: center;
       display: flex;
       padding: 2em;
-      .conten-img{
-        padding-left: 0.5rem;
+      .content-img {
+        padding-left: 0.5em;
 
-        .wrapper-img{
+        .wrapper-img {
           position: relative;
           margin: auto;
           //box-shadow: 2px 2px 5px #999;
@@ -58,17 +66,14 @@ text-align: center;
           //  right: 0;
           //  background: black;
           //}
-          .clicked{
-            
+          .clicked {
           }
-          .not-clicked{
-            opacity: 0.10;
+          .not-clicked {
+            opacity: 0.1;
             transition: all 0.2s ease-in-out;
           }
         }
-        
       }
-      
     }
   }
-`
+`;
