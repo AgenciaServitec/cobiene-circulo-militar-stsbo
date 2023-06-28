194cd1426245a781a71a3279691908b364d0c62d
angelgt <angel.gamonal@agenciaservitec.com>
2023-06-28 18:47:46 -0500

    remove import React because never used

--- src/hooks/useFormUtils.js
+++ src/hooks/useFormUtils.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { get, isEmpty, isString, toNumber } from "lodash";
 
 export const useFormUtils = ({ errors, schema }) => {
