a65cd772698acdcb3444b555547dd74ce29f4b3b
angelgt <angel.gamonal@agenciaservitec.com>
2023-07-20 12:55:34 -0500

    remove images

--- src/data-list/galleryData.js
+++ src/data-list/galleryData.js
@@ -7,27 +7,17 @@ import {
   ImageGalleyHome3,
   ImageGalleyHome4,
   ImageGalleyHome5,
-  ImageGalleyHome6,
   ImageGalleyHome9,
 } from "../images";
 
 export const galleryData = [
-  //{ src: ImageGalleyHome1, title: "Reconocimiento al Personal Militar" },
-  // { src: ImageGalleyHome16, title: "Sobre nosotros" },
   { src: ImageGalleyHome3, title: "Areas de Ceremonias" },
   { src: ImageGalleyHome4, title: "Piscinas de las Sedes" },
   { src: ImageGalleyHome5, title: "Piscinas de las Sedes" },
-  { src: ImageGalleyHome6, title: "Piscinas de las Sedes" },
-
-  /*  {src:ImageGalleyHome8,title:""},*/
-
   { src: ImageGalleyHome9, title: "Piscinas de las Sedes" },
   { src: ImageGalleyHome10, title: "Areas Sociales" },
   { src: ImageGalleyHome11, title: "Areas de sociales" },
-  //{src:ImageGalleyHome15, title: "Presidente"},
   { src: ImageGalleyHome12, title: "Areas de Eventos" },
   { src: ImageGalleyHome13, title: "oficinas sede chorrillos" },
   { src: ImageGalleyHome14, title: "oficinas sede chorrillos" },
-  { src: ImageGalleyHome12, title: "Areas de Eventos" },
-  { src: ImageGalleyHome3, title: "Areas de Ceremonias" },
 ];
